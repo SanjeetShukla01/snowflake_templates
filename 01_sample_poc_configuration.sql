c786957319ccce24c5420e7012c8fad303c2c9b8
SanjeetShukla01 <sanjeet.shukla089@gmail.com>
2024-06-30 13:58:27 +0530

    poc template 1

---
+++ 01_sample_poc_configuration.sql
@@ -0,0 +1,51 @@
+SET poc_database       = 'POC';
+SET poc_owner_schema   = 'POC.poc_owner';
+SET poc_warehouse      = 'poc_wh';
+SET poc_owner_role     = 'poc_owner_role';
+
+USE ROLE sysadmin;
+
+CREATE OR REPLACE DATABASE IDENTIFIER ( $poc_database ) DATA_RETENTION_TIME_IN_DAYS = 7;
+
+CREATE OR REPLACE WAREHOUSE IDENTIFIER ( $poc_warehouse ) WITH
+WAREHOUSE_SIZE      = 'X-SMALL'
+AUTO_SUSPEND        = 60
+AUTO_RESUME         = TRUE
+MIN_CLUSTER_COUNT   = 1
+MAX_CLUSTER_COUNT   = 4
+SCALING_POLICY      = 'STANDARD'
+INITIALLY_SUSPENDED = TRUE;
+
+If your POC involves Snowpark, you may want to add a WAREHOUSE_TYPE ='SNOWPARK-OPTIMIZED' to have a bigger memory allocation to handle large datasets.
+
+
+CREATE OR REPLACE SCHEMA IDENTIFIER ($poc_owner_schema);
+
+USE ROLE securityadmin;
+CREATE OR REPLACE ROLE IDENTIFIER ($poc_owner_role) COMMENT ='POC.poc_owner Role';
+
+GRANT ROLE IDENTIFIER ( $poc_owner_role  ) TO ROLE securityadmin;
+
+
+GRANT USAGE   ON DATABASE  IDENTIFIER ($poc_database ) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT USAGE   ON WAREHOUSE IDENTIFIER ($poc_warehouse) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT MONITOR ON WAREHOUSE IDENTIFIER ($poc_warehouse) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT OPERATE ON WAREHOUSE IDENTIFIER ($poc_warehouse) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT USAGE   ON SCHEMA    IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT USAGE   ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT MONITOR ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+
+
+GRANT MODIFY                     ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT CREATE TABLE               ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT CREATE VIEW                ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT CREATE SEQUENCE            ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT CREATE FUNCTION            ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT CREATE PROCEDURE           ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT CREATE STREAM              ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT CREATE MATERIALIZED VIEW   ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT CREATE FILE FORMAT         ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+GRANT CREATE TAG                 ON SCHEMA IDENTIFIER ($poc_owner_schema) TO ROLE IDENTIFIER ($poc_owner_role);
+
+
+
